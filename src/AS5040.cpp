9aaca310f15ca9ee9a3e2b5378138afd8027d5ed
Federico D. Ceccarelli <fededc88@gmail.com>
2025-01-26 10:51:38 -0500

    first sketch of src files

--- src/AS5040.cpp
+++ src/AS5040.cpp
@@ -1,6 +1,27 @@
 
 #include "AS5040.h"
 
+enum AS5040_RC AS5040::begin()
+{
+    enum AS5040_RC rc; /* return command */
+
+    return rc;
+}
+
+uint16_t readAbsolutePosition(void)
+{
+    uint16_t val;
+    //TODO:
+
+    return val;
+}
+
+uint16_t _write_read(uint16_t write_val)
+{
+
+    //TODO:
+}
+
 // 
 // End of file.
 //
--- src/AS5040.h
+++ src/AS5040.h
@@ -1,6 +1,26 @@
 #ifndef __AS5040_H__
 #define __AS5040_H__
 
+#include <stdint.h>
+
+enum AS5040_RC
+{
+    AS5040_FAIL = -1,
+    AS5040_OK = 0
+};
+
+class AS5040 
+{
+public:
+    enum AS5040_RC begin();
+    uint16_t readAbsolutePosition(void);
+
+private:
+    uint16_t _write_read(uint16_t write_val);
+};
+
 #endif
 
+//
 // End of file.
+//
